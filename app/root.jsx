f6449884acb8aa61b510950833b7320cd70cccf3
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-04 13:52:51 -0300

    Creando app y document para layout principal

---
+++ app/root.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+    return (
+        <Document>
+            <h1>Hola mundo</h1>
+        </Document>
+    )
+}
+
+function Document({children}) {
+    return (
+        <html lang="es">
+            <head>
+                <title>GuitarLA - Remix</title>
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
