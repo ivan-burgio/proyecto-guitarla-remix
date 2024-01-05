d4fd52ed40e7258a32d1d6855f090fd334629cd9
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-05 10:56:06 -0300

    Añadiendo informacion meta

--- app/root.jsx
+++ app/root.jsx
@@ -1,3 +1,15 @@
+import {
+    Meta
+} from '@remix-run/react'
+
+export function meta() {
+    return [
+      { charset: "utf-8" },
+      { title: "GuitarLA - Remix" },
+      { name: "viewport", content: "width=device-width,initial-scale=1" },
+    ];
+  }
+
 export default function App() {
     return (
         <Document>
@@ -10,7 +22,7 @@ function Document({children}) {
     return (
         <html lang="es">
             <head>
-                <title>GuitarLA - Remix</title>
+                <Meta />
             </head>
             <body>
                 {children}
