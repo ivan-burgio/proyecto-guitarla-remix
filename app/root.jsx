303f8edf7a078319172635c7f54e2ef8758f47a0
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-05 11:17:57 -0300

    Agrgando hojas de estilos con Links

--- app/root.jsx
+++ app/root.jsx
@@ -1,32 +1,44 @@
-import {
-    Meta
-} from '@remix-run/react'
+import { 
+    Meta,
+    Links,
+} from "@remix-run/react";
+import styles from './styles/index.css';
 
 export function meta() {
     return [
-      { charset: "utf-8" },
-      { title: "GuitarLA - Remix" },
-      { name: "viewport", content: "width=device-width,initial-scale=1" },
+        { charset: "utf-8" },
+        { title: "GuitarLA - Remix" },
+        { name: "viewport", content: "width=device-width,initial-scale=1" },
     ];
-  }
+}
+
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
 
 export default function App() {
     return (
         <Document>
             <h1>Hola mundo</h1>
         </Document>
-    )
+    );
 }
 
-function Document({children}) {
+function Document({ children }) {
     return (
         <html lang="es">
             <head>
                 <Meta />
+                <Links />
             </head>
             <body>
                 {children}
             </body>
         </html>
-    )
+    );
 }
