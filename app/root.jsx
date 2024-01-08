d892babece140e55780fce9709bee5ee7e4ce8d6
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-08 12:25:47 -0300

    Creando el header y el Alias hacia app

--- app/root.jsx
+++ app/root.jsx
@@ -3,7 +3,8 @@ import {
     Links,
     Outlet,
 } from "@remix-run/react";
-import styles from './styles/index.css';
+import styles from '~/styles/index.css';
+import Header from "~/components/header";
 
 export function meta() {
     return [
@@ -55,6 +56,7 @@ function Document({ children }) {
                 <Links />
             </head>
             <body>
+                <Header />
                 {children}
             </body>
         </html>
