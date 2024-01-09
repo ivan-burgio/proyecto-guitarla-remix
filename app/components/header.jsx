cbe4c190069a14871f125db02da045db333009cf
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-09 13:17:38 -0300

    Creadno el footer

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,9 +1,8 @@
-import { Link, useLocation } from '@remix-run/react';
+import { Link } from '@remix-run/react';
 import logo from '../../public/img/logo.svg';
+import Navegacion from './navegacion';
 
 export default function Header() {
-    const location = useLocation();
-
     return (
         <header className="header">
             <div className="contenedor barra">
@@ -11,27 +10,7 @@ export default function Header() {
                     <img className='logo' src={logo} alt="imagen logo" />
                 </Link>
 
-                <nav className="navegacion">
-                    <Link
-                        to="/"
-                        className={location.pathname === '/' ? 'active' : ''}
-                    >Inicio</Link>
-                    
-                    <Link
-                        to="/nosotros"
-                        className={location.pathname === '/nosotros' ? 'active' : ''}
-                    >Nosotros</Link>
-
-                    <Link
-                        to="/tienda"
-                        className={location.pathname === '/tienda' ? 'active' : ''}
-                    >Tienda</Link>
-
-                    <Link
-                        to="/blog"
-                        className={location.pathname === '/blog' ? 'active' : ''}
-                    >Blog</Link>
-                </nav>
+                <Navegacion />
             </div>
         </header>
     )
