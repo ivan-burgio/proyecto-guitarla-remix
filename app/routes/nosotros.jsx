825b38217ed507ea119faf67bb6f252819621036
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-10 13:40:10 -0300

    CSS a la pagina de nosotros y otros atributos a meta

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,21 +1,32 @@
 import imagen from '../../public/img/nosotros.jpg';
+import styles from '~/styles/nosotros.css';
 
 export function meta() {
     return [
-        { title: "GuitarLA - Nosotros" },
+        {
+            title: "GuitarLA - Sobre Nosotros",
+            description: 'Venta de gruitarras, blog de música.'
+        },
     ];
 }
 
 export function links() {
-    return [{
-        rel: "stylesheet", 
-        href: "header.css", 
-    }];
+    return [
+        {
+            rel: "stylesheet", 
+            href: styles, 
+        },
+        {
+            rel: 'preload',
+            href: imagen,
+            as: 'image'
+        }
+    ];
 }
 
 export default function Nosotros() {
   return (
-    <main className="contenedir nosotros">
+    <main className="contenedor nosotros">
         <h2 className="heading">Nosotros</h2>
 
         <div className="contenido">
@@ -23,10 +34,10 @@ export default function Nosotros() {
 
             <div>
                 <p>
-                    Ut consectetur porttitor velit, eu consequat nibh varius et. Morbi suscipit ullamcorper consectetur. Maecenas auctor turpis pretium lacus rutrum efficitur. Morbi a purus semper, suscipit lectus in, dictum purus. Aenean non erat eu justo eleifend tincidunt. Sed lobortis consequat nulla, quis facilisis lectus cursus nec. Nam orci est, gravida nec maximus at, finibus vitae mauris. Vestibulum placerat iaculis ante volutpat sodales. Donec tristique varius dapibus. Donec dignissim dictum finibus. Quisque dignissim, est non consectetur egestas, orci odio rhoncus dui, a mollis mi nisi non nibh. Praesent at dolor fermentum, bibendum neque aliquam, pellentesque arcu. Etiam sit amet aliquet dolor, bibendum dignissim lacus. Mauris aliquet dolor convallis dignissim rhoncus. Suspendisse ultricies, purus eu pellentesque tincidunt, augue dui dictum turpis, at bibendum risus elit nec velit. 
+                    Ut consectetur porttitor velit, eu consequat nibh varius et. Morbi suscipit ullamcorper consectetur. Maecenas auctor turpis pretium lacus rutrum efficitur. Morbi a purus semper, suscipit lectus in, dictum purus. Aenean non erat eu justo eleifend tincidunt. Sed lobortis consequat nulla, quis facilisis lectus cursus nec. Nam orci est, gravida nec maximus at, finibus vitae mauris. 
                 </p>
                 <p>
-                    Ut consectetur porttitor velit, eu consequat nibh varius et. Morbi suscipit ullamcorper consectetur. Maecenas auctor turpis pretium lacus rutrum efficitur. Morbi a purus semper, suscipit lectus in, dictum purus. Aenean non erat eu justo eleifend tincidunt. Sed lobortis consequat nulla, quis facilisis lectus cursus nec. Nam orci est, gravida nec maximus at, finibus vitae mauris. Vestibulum placerat iaculis ante volutpat sodales. Donec tristique varius dapibus. Donec dignissim dictum finibus. Quisque dignissim, est non consectetur egestas, orci odio rhoncus dui, a mollis mi nisi non nibh. Praesent at dolor fermentum, bibendum neque aliquam, pellentesque arcu. Etiam sit amet aliquet dolor, bibendum dignissim lacus. Mauris aliquet dolor convallis dignissim rhoncus. Suspendisse ultricies, purus eu pellentesque tincidunt, augue dui dictum turpis, at bibendum risus elit nec velit. 
+                    Vestibulum placerat iaculis ante volutpat sodales. Donec tristique varius dapibus. Donec dignissim dictum finibus. Quisque dignissim, est non consectetur egestas, orci odio rhoncus dui, a mollis mi nisi non nibh. Praesent at dolor fermentum, bibendum neque aliquam, pellentesque arcu. Etiam sit amet aliquet dolor, bibendum dignissim lacus. Mauris aliquet dolor convallis dignissim rhoncus. Suspendisse ultricies, purus eu pellentesque tincidunt, augue dui dictum turpis, at bibendum risus elit nec velit. 
                 </p>
             </div>
         </div>
