fbf63a6dc8708e66fdb1286cc44eb75fb6f8cbe1
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-10 14:05:14 -0300

    Consumiendo el backend de strapi con un loader

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,3 +1,9 @@
+export async function loader() {
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+    const resultado = await respuesta.json();
+    return {};
+}
+
 export default function Tienda() {
   return (
     <div>tienda</div>
