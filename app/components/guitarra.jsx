0119ff9609baacf9a50aa06d7640a609be1fc985
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-11 11:23:10 -0300

    Mostrando la informacion de la guitarra

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Link } from "@remix-run/react";
 
 export default function Guitarra({guitarra}) {
+    const { descripcion, imagen, precio, url, nombre } = guitarra;
+
     return (
-        <div>guitarra</div>
+        <div className='guitarra'>
+            <img src={imagen.data.attributes.formats.medium.url} alt={`Imagen guitarra ${nombre}`} />
+
+            <div className='contenido'>
+                <h3>{nombre}</h3>
+                <p className='descripcion'>{descripcion[0].children[0].text}</p>
+                <p className='precio'>${precio}</p>
+            </div>
+
+            <Link className="enlace" to={`/guitarras/${url}`}>Ver Producto</Link>
+        </div>
     )
 }
