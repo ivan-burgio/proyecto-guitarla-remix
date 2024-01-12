c65c2eeeea9d03679f483f79da208a02511f30e6
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-12 12:21:38 -0300

    Aplicadno CSS al listado de Guitarras

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,25 @@
 import { useLoaderData } from '@remix-run/react';
 import { getGuitarras } from '~/models/guitarras.server';
 import Guitarra from '~/components/guitarra';
+import styles from '~/styles/guitarras.css';
+
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles,
+        }
+    ]
+}
+
+export function meta() {
+    return [
+        {
+            title: "GuitarLA - Tienda de Guitarras",
+            description: 'Nuestra coleccion de guitarras.'
+        },
+    ];
+}
 
 export async function loader() {
     const guitarras = await getGuitarras();
