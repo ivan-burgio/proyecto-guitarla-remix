39af1a10bc1b565d61b8967a423e30e1d5e8e655
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-12 13:30:19 -0300

    filtrar las guitarras por la url

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,13 +1,19 @@
+import { getGuitarra } from "../models/guitarras.server";
+import { useLoaderData } from "@remix-run/react";
+
 export async function loader({request, params}) {
     const { guitarraUrl } = params;
+    const guitarra = await getGuitarra(guitarraUrl);
 
-    return {};
+    return guitarra;
 }
 
 export default function Guitarra() {
+    const guitarra = useLoaderData();
+
     return (
         <div>
-
+            {guitarra.data[0].attributes.nombre}
         </div>
     )
 }
