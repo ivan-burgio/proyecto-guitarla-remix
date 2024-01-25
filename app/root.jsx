100187b625c90243c2a03b994b2b171bc5c0539d
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-25 12:23:57 -0300

    Evitar registros duplicados

--- app/root.jsx
+++ app/root.jsx
@@ -49,15 +49,33 @@ export function links() {
 export default function App() {
     const [carrito, setCarrito] = useState([]);
 
-    const agregarCarrito = guitarra => {
-        setCarrito([...carrito, guitarra]);
-    }
+    const agregarCarrito = (guitarra) => {
+        if (carrito.some((guitarraState) => guitarraState.id === guitarra.id)) {
+            // Iterar sobre el arreglo e identificar el elemento duplicado
+            const carritoActualizado = carrito.map((guitarraState) => {
+                if (guitarraState.id === guitarra.id) {
+                    // Reescribir la cantidad
+                    guitarraState.cantidad = guitarra.cantidad;
+                }
+
+                return guitarraState;
+            });
+
+            // Añadir al carrito
+            setCarrito(carritoActualizado);
+        } else {
+            // Registro nuevo, agregar al carrito
+            setCarrito([...carrito, guitarra]);
+        }
+    };
 
     return (
         <Document>
-            <Outlet context={{
-                agregarCarrito
-            }} />
+            <Outlet
+                context={{
+                    agregarCarrito,
+                }}
+            />
         </Document>
     );
 }
