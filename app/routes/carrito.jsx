a3caf0ff87dd33ab7faddb7554eab959a8f068f6
ivan-burgio <ivan.24.burgio@gmail.com>
2024-01-26 12:55:37 -0300

    Identacion del codigo

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -33,52 +33,51 @@ export default function Carrito() {
                     {carrito && carrito.length === 0
                         ? "Carrito Vacio"
                         : carrito &&
-                          carrito.map((producto) => (
-                              <div key={producto.id} className="producto">
-                                  <div>
-                                      <img
-                                          src={producto.imagen}
-                                          alt={`imagen del producto ${producto.nombre}`}
-                                      />
-                                  </div>
+                        carrito.map((producto) => (
+                            <div key={producto.id} className="producto">
+                                <div>
+                                    <img
+                                        src={producto.imagen}
+                                        alt={`imagen del producto ${producto.nombre}`}
+                                    />
+                                </div>
 
-                                  <div>
-                                      <p className="nombre">
-                                            {producto.nombre}
-                                      </p>
-                                      <p className="cantidad">Cantidad: </p>
+                                <div>
+                                    <p className="nombre">
+                                        {producto.nombre}
+                                    </p>
+                                    <p className="cantidad">Cantidad: </p>
 
-                                      <select
-                                            value={producto.cantidad}
-                                            className="select"
-                                            onChange={(e) =>
-                                                actualizarCantidad({
-                                                    cantidad: +e.target.value,
-                                                    id: producto.id,
-                                                })
-                                            }
-                                      >
-                                          <option value="1">1</option>
-                                          <option value="2">2</option>
-                                          <option value="3">3</option>
-                                          <option value="4">4</option>
-                                          <option value="5">5</option>
-                                      </select>
+                                    <select
+                                        value={producto.cantidad}
+                                        className="select"
+                                        onChange={(e) =>
+                                            actualizarCantidad({
+                                                cantidad: +e.target.value,
+                                                id: producto.id,
+                                            })
+                                        }
+                                    >
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
 
-                                      <p className="precio">
-                                          $<span>{producto.precio}</span>
-                                      </p>
+                                    <p className="precio">
+                                        $<span>{producto.precio}</span>
+                                    </p>
 
-                                      <p className="subtotal">
-                                          Subtotal: $
-                                          <span>
-                                              {producto.cantidad *
-                                                  producto.precio}
-                                          </span>
-                                      </p>
-                                  </div>
-                              </div>
-                          ))}
+                                    <p className="subtotal">
+                                        Subtotal: $
+                                        <span>
+                                            {producto.cantidad * producto.precio}
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
                 </div>
 
                 <aside className="resumen">
